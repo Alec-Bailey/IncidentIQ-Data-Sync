622aa26c7b029a293d45b63bd2c70ba2a88faa59
Alec Bailey <alecj.bailey@gmail.com>
2021-06-17 18:03:14 -0500

    Added pool_size parameter to engine to match the max number of threads which can be spawned at once

--- base.py
+++ base.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import config
 
-engine = sqlalchemy.create_engine(config.DB_CONNECTION_STRING)
+engine = sqlalchemy.create_engine(config.DB_CONNECTION_STRING, pool_size=config.THREADS, max_overflow=0)
 
 Session = sessionmaker(bind=engine)
 
