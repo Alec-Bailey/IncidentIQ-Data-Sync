38e9fa9a5c07f5356d300370a1f4b8735ae4e295
Alec Bailey <alecj.bailey@gmail.com>
2021-06-17 18:25:07 -0500

    Added config.py Reads data from config.ini into variables for easy configuration

---
+++ config.py
@@ -0,0 +1,15 @@
+from configparser import ConfigParser
+
+cf = ConfigParser()
+cf.read('config.ini')
+
+# Local Database for backup
+DATABASE = cf.get('Database', 'Database')
+DB_CONNECTION_STRING = cf.get('Database', 'ConnectionString')
+# Incident IQ Credentials
+IIQ_INSTANCE = cf.get('IncidentIQ', 'Instance')
+IIQ_TOKEN = cf.get('IncidentIQ', 'Token')
+# The number of elements to request in each call to the API, can be tuned down for low memory machines (slower)
+# or up for servers which can handle the load (faster).
+PAGE_SIZE = cf.get('General', 'PageSize')
+THREADS = cf.get('General', 'Threads')
