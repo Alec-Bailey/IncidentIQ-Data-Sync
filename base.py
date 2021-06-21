1d43c3e48a741118df63042c0d1ebd91ea609a73
Alec Bailey <alecj.bailey@gmail.com>
2021-06-21 16:11:37 -0500

    Implemented IIQ_Datatype as an abstract class. Acts as an interface for all IIQ types to conform to. Changed session to a scoped_session for thread safety.

--- base.py
+++ base.py
@@ -1,11 +1,28 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 import config
 
 engine = sqlalchemy.create_engine(config.DB_CONNECTION_STRING, pool_size=int(config.THREADS), max_overflow=0)
 
-Session = sessionmaker(bind=engine)
+# Create a scoped session for thread safety, attempting to commit multiple sessions
+# at once
+Session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
+
+
+class IIQ_Datatype:
+
+    @staticmethod
+    def __get_data_request(page_number):
+        raise NotImplementedError("__get_data_request API Request not implemented")
+
+    @staticmethod
+    def get_num_pages():
+        raise NotImplementedError("get_num_pages not impelmeneted")
+
+    @staticmethod
+    def get_page(page_number):
+        raise NotImplementedError("get_page not implemented")
