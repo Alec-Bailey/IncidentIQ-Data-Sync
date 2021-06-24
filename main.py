9c5e8cddd294e434e96ac1d0967461afdca8a40d
Alec Bailey <alecj.bailey@gmail.com>
2021-06-24 16:28:52 -0500

    Added support for dynamic table generation to support Custom Fields

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from threading import Lock
 from sqlalchemy.sql.sqltypes import DateTime
 from base import Session, engine, Base, IIQ_Datatype
 from user import User
+from location import Location
+from custom_fields import UserCustomFields, AssetCustomFields
 import config
 
 __author__ = "Alec Bailey"
@@ -25,17 +27,24 @@ __maintainer__ = "Alec Bailey"
 __email__ = "alecj.bailey@gmail.com"
 __status__ = "Development"
 
+def __generate_custom_fields_tables():
+    UserCustomFields.create_table('UserCustomFields', 'UserId')
+    AssetCustomFields.create_table('AssetCustomFields', 'AssetId')
+
+
 # One unit of work, executed by a thread. Creates a session and preforms a web
 # request to the IncidentIQ API. Inserts the returned elements into the
 # appropriate database table, and commits the changes.
 def __sync_object(cls : IIQ_Datatype, index):
     session = Session()
-
+    
     # Retrieve an entire API Page worth of objects
     page = cls.get_page(index)
 
+
     # Add each object to the session and commit it
     session.add_all(page)
+    
 
     session.commit() #TODO: this is causing a reace condition on commit?? changed to threadsafe version?
     session.close()
@@ -44,6 +53,7 @@ def __sync_object(cls : IIQ_Datatype, index):
 def __execute_sync(IIQ_Type : IIQ_Datatype):
     # Pull all users down into database
     num_pages = IIQ_Type.get_num_pages()
+
     # Create a thread pool with config.THREADS number of threads. This calls __sync_object
     # for each page of the API we wish to request, from page 0 to num_pages. Each thread
     # is responsible for adding exactly one page of the API response (by default 1000 objects) 
@@ -56,11 +66,16 @@ def __execute_sync(IIQ_Type : IIQ_Datatype):
     
 if __name__ == '__main__':
     start_time = time.time() #TODO: remove
+    __generate_custom_fields_tables()
     # Generate database schema from SqlAlchemy
     Base.metadata.create_all(engine)
 
     # Execute the Users sync
-    __execute_sync(User)
+    #__execute_sync(User)
+
+    #__execute_sync(Location)
+
+
 
     stop_time = time.time() #TODO: remove
 
