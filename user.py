a67b537130c8a2e6d4fd64332f8edde93673f604
Alec Bailey <alecj.bailey@gmail.com>
2021-06-28 16:31:47 -0500

    Fixed an issue where @validates was using fields.__hash__ instead of *fields, which is what is actually nescessary to unpack fields. Declared mapping in __init__ to setattr iterating over fields for simplicity. Added get_custom_type Added _get_custom_fields

--- user.py
+++ user.py
@@ -1,4 +1,12 @@
-# User class respresents IncidentIQ users
+#!/usr/bin/env python
+"""user.py: Declaratively mapped User from IncidentIQ
+
+User contains the methods nescessary to pull and transofrm
+the data from the IncidentIQ to conform to declaratively mapped
+object in SqlAlcemy. User can be instantiated with data from
+IncidentIQ to insert into the specified database.
+"""
+
 from requests.models import HTTPError
 from sqlalchemy import Column, String, Integer, Date, Boolean
 from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER as UNIQUEIDENTIFIER # TODO: we can use this import statement in a switch to support multiple dbs
@@ -10,11 +18,19 @@ import requests
 from types import SimpleNamespace as Namespace
 
 class User(Base, IIQ):
+    """User is an instanciable class which holds all the information
+    for one User in IncidentIQ. User also contains methods to make an
+    API request to retreive an entire page of Users as well as
+    create an instance of the dynamic UserCustomFields class.
+    User is declaratively mapped in SqlAlchemy to the 'Users' table,
+    on instantiation Asset can be inserted into 'Users' via an SqlAlchemy
+    session."""
+
     __tablename__ = 'Users'
     __table_args__ = {'schema': config.SCHEMA}
 
     # Retreive custom fields
-    custom_fields = UserCustomFields.parse_fields()
+    custom_fields = UserCustomFields.parse_fields(UserCustomFields.get_fields_request(0))
 
     #TODO: using UNIQUEIDENTIFER at all, especially as it's correct type and PK presents an interesting
     # challenge, SqlAlchemy doesn't support a database agnostic way of dealing with this
@@ -50,53 +66,30 @@ class User(Base, IIQ):
     IsOutOfOffice = Column(Boolean)
     Portal = Column(Integer)
 
-    fields = ['UserId','IsDeleted','SiteId','CreatedDate','ModifiedDate',
-    'LocationId','LocationName','IsActive','IsOnline','IsOnlineLastUpdated',
-    'FirstName','LastName','Email','Username','Phone','SchoolIdNumber','Grade',
-    'Homeroom','ExternalId','InternalComments','RoleId','AuthenticatedBy',
-    'AccountSetupProgress','TrainingPercentComplete','IsEmailVerified',
-    'IsWelcomeEmailSent','PreventProviderUpdates','IsOutOfOffice','Portal']
+    fields = ['AccountSetupProgress', 'AuthenticatedBy', 'CreatedDate', 
+    'Email', 'ExternalId', 'FirstName', 'Grade', 'Homeroom', 
+    'InternalComments', 'IsActive', 'IsDeleted', 'IsEmailVerified',
+     'IsOnline', 'IsOnlineLastUpdated', 'IsOutOfOffice', 'IsWelcomeEmailSent',
+      'LastName', 'LocationId', 'LocationName', 'ModifiedDate', 'Phone',
+       'Portal', 'PreventProviderUpdates', 'RoleId', 'SchoolIdNumber', 
+       'SiteId', 'TrainingPercentComplete', 'UserId', 'Username']
 
     # Validator ensures empty strings are entered as null
-    @validates(fields.__hash__)
+    @validates(*fields)
     def empty_string_to_null(self, key, value):
-        if isinstance(value, str) and value == '':
-            return None
-        else:
-            return value
+        return super().empty_string_to_null(key, value)
 
     def __init__(self, data):
-        # Extract fields from the raw data, optional fields are retrieved
-        # safely via find_element
-        self.UserId = data.UserId
-        self.IsDeleted = IIQ.find_element(data, 'IsDeleted')
-        self.SiteId = data.SiteId
-        self.CreatedDate = data.CreatedDate
-        self.ModifiedDate = data.ModifiedDate
-        self.LocationId = data.LocationId
-        self.LocationName = IIQ.find_element(data, 'LocationName')
-        self.IsActive = data.IsActive
-        self.IsOnline = IIQ.find_element(data, 'IsOnline')
-        self.IsOnlineLastUpdated = IIQ.find_element(data, 'IsOnlineLastUpdated')
-        self.FirstName = IIQ.find_element(data, 'FirstName')
-        self.LastName = IIQ.find_element(data, 'LastName')
-        self.Email = IIQ.find_element(data, 'Email')
-        self.Username = IIQ.find_element(data, 'Username')
-        self.Phone = IIQ.find_element(data, 'Phone')
-        self.SchoolIdNumber = IIQ.find_element(data, 'SchoolIdNumber')
-        self.Grade = IIQ.find_element(data, 'Grade')
-        self.Homeroom = IIQ.find_element(data, 'Homeroom')
-        self.ExternalId = IIQ.find_element(data, 'ExternalId')
-        self.InternalComments = IIQ.find_element(data, 'InternalComments')
-        self.RoleId = data.RoleId
-        self.AuthenticatedBy = IIQ.find_element(data, 'AuthenticatedBy')
-        self.AccountSetupProgress = data.AccountSetupProgress
-        self.TrainingPercentComplete = data.TrainingPercentComplete
-        self.IsEmailVerified = data.IsEmailVerified
-        self.IsWelcomeEmailSent = IIQ.find_element(data, 'IsWelcomeEmailSent')
-        self.PreventProviderUpdates = data.PreventProviderUpdates
-        self.IsOutOfOffice = IIQ.find_element(data, 'IsOutOfOffice')
-        self.Portal = data.Portal
+        # Extract fields from the raw data, optional nested fields
+        # can be retrieved easily with find_element *args (See asset.py)
+        for field in self.fields:
+            # For non-nested fields that exist at the first level of the JSON
+            # we can use setattr to assign values, since the fields are
+            # named exactly as they appear in the JSON. For example, an asset
+            # JSON response will have a field 'AssetId' at the base level of that item. 
+            # Thus, find_element can grab it simply by being passed 'AssetId'. By design,
+            # the column is also named 'AssetId', so we can iterate simply and set these fields.
+            setattr(self, field, IIQ.find_element(data, field))
 
     @staticmethod
     def get_data_request(page):
@@ -115,7 +108,7 @@ class User(Base, IIQ):
         }
         
         response =  requests.request("POST", url, headers=headers, data=payload, files=files)
-
+        
         # Cause an exception if anything but success is returned
         if response.status_code != 200:
             raise HTTPError("""A request returned a status code other than 200\n
@@ -128,12 +121,18 @@ class User(Base, IIQ):
         # Return the response
         return response
 
-    # Retrieve the number of pages to iterate through
     @staticmethod
     def get_num_pages():
         return User.get_data_request(0).json()['Paging']['PageCount']
 
-    # Retreives all users from a request page and returns a list of all as User objects
     @classmethod
     def get_page(cls, page_number):
         return super().get_page(page_number)
+
+    @staticmethod
+    def get_custom_type():
+        return UserCustomFields
+
+    @classmethod
+    def _get_custom_fields(cls, item):
+        return super()._get_custom_fields(item)
