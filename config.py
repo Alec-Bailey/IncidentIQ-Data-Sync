00a8274182c9ef6469e6dc8d526460e2c7c36f21
Alec Bailey <alecj.bailey@gmail.com>
2021-06-29 19:41:53 -0500

    Removed 'Database' field, as it was not used.

--- config.py
+++ config.py
@@ -12,15 +12,13 @@ cf = ConfigParser()
 cf.read('config.ini')
 
 # Local Database for backup
-DATABASE = cf.get('Database', 'Database')
 DB_CONNECTION_STRING = cf.get('Database', 'ConnectionString')
 SCHEMA = cf.get('Database', 'Schema')
 STRING_LENGTH = int(cf.get('Database', 'StringLength'))
 # Incident IQ Credentials
 IIQ_INSTANCE = cf.get('IncidentIQ', 'Instance')
 IIQ_TOKEN = cf.get('IncidentIQ', 'Token')
-# The number of elements to request in each call to the API, can be tuned down for low memory machines (slower)
-# or up for servers which can handle the load (faster).
+# General configuration
 PAGE_SIZE = cf.get('General', 'PageSize')
 THREADS = cf.get('General', 'Threads')
 TIMEOUT = int(cf.get('General', 'Timeout'))
