ccb9d2cefad182840e0ee50b40573f1d411a07cb
Alec Bailey <alecj.bailey@gmail.com>
2021-07-09 18:50:05 -0500

    Changed how the SCHEMA variable is read. Now conditionally set to None if a blank string or nothing is specified in the config.ini This allows us to prevent issues with empty string schemas, and strange incompatibility issues with databases that do not support schemas

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ cf.read('config.ini')
 
 # Local Database for backup
 DB_CONNECTION_STRING = cf.get('Database', 'ConnectionString')
-SCHEMA = cf.get('Database', 'Schema')
+SCHEMA = cf.get('Database', 'Schema') if len(cf.get('Database', 'Schema').strip()) > 0 else None  # Assign to none if schema is blank 
 STRING_LENGTH = int(cf.get('Database', 'StringLength'))
 # Table Names
 ASSETS_TABLE_NAME = cf.get('Tables', 'Assets')
